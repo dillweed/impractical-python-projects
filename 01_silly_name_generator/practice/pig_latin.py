3050b30dff0e838a5e08e9da8e0c8f3821b6093e
Dill <4053467+dillweed@users.noreply.github.com>
2022-11-19 14:57:39 -0800

    Added TODOs

--- 01_silly_name_generator/practice/pig_latin.py
+++ 01_silly_name_generator/practice/pig_latin.py
@@ -5,10 +5,11 @@ To form Pig Latin, you take an English word that begins with a consonant,
 move that consonant to the end, and then add “ay” to the end of the word.
 If the word begins with a vowel, you simply add “way” to the end of the word.
 """
-# TODO preserve '.'
+# TODO preserve punctuation.
+# TODO add edge rule for words with no vowel but 'y' like 'my'.
 
 
-def main():
+def main() -> None:
     """Convert input to pig-latin."""
     print("\n" + convert_to_pig_latin(get_input()) + "\n")
 

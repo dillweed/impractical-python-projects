61a2474b6dfe50489685017d0168f7243c6415ea
Dill <4053467+dillweed@users.noreply.github.com>
2022-11-20 13:51:37 -0800

    Added working finding_palindromes.py draft

--- 02_finding_palingram_spells/finding_palindromes.py
+++ 02_finding_palingram_spells/finding_palindromes.py
@@ -1,19 +1,54 @@
-"""TBA"""
+"""TBA."""
 import sys
+
+FILE_PATH: str = "inventwithpython_dictionary.txt"
 
 
 def main():
-    """TBA"""
+    """TBA."""
+    # Read dictionary file
+    word_list: list = open_file(FILE_PATH)
+
+    # Make a list of palindromes
+    palindromes: list = []
+    for word in word_list:
+        if is_pal(word):
+            palindromes.append(word)
+
+    # Print palindrome list
+    print(f"\n{len(palindromes)} palindromes found.\n\n", palindromes)
+
+
+def open_file(file: str) -> list:
+    """Open file and return list of lines in lowercase.
+
+    Args:
+        FILE_PATH (str): The spelling dictionary file
+
+    Returns:
+        words (list): Word list in lowercase
+    """
     # Read dictionary file with try block
     try:
-        with open("inventwithpython_dictionary.txt", "r", encoding="utf-8") as dict_file:
-            spell_dict = dict_file.read().splitlines()
+        with open(file, "r", encoding="utf-8") as word_file:
+            word_list: list = word_file.read().lower().splitlines()
 
     except IOError as _e:
         print(_e)
         sys.exit(1)
+    return word_list
 
-    print(spell_dict)
+
+def is_pal(word: str) -> bool:
+    """Return bool if word is a palindrome.
+
+    Args:
+        word (str): Word to test
+
+    Returns:
+        bool: True if word is a palindrome
+    """
+    return word == word[::-1]
 
 
 if __name__ == "__main__":
